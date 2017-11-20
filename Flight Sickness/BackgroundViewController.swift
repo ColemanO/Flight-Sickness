11987d9c26119185d0433e75c730b860203ec15a
Coleman Oei <colemanoei@wireless-10-145-241-65.public.utexas.edu>
2017-11-20 16:33:34 -0600

    cleaned the leaderboard UI a bit

--- Flight Sickness/BackgroundViewController.swift
+++ Flight Sickness/BackgroundViewController.swift
@@ -22,6 +22,7 @@ class BackgroundViewController: UIViewController, UITableViewDelegate, UITableVi
         //cell.textLabel?.font = UIFont.init(name: "Avenir-Heavy", size: 20)
         //cell.detailTextLabel?.font = UIFont.init(name: "Avenir-Heavy", size: 20)
         if (indexPath.row < DataStore.shared.count()) {
+            cell.textLabel?.font = UIFont(name:"DCCHardware-Condensed", size:22)
             cell.textLabel?.text = DataStore.shared.getUser(index: indexPath.row).username
             cell.detailTextLabel?.text = DataStore.shared.getUser(index: indexPath.row).password //Need to change this to score
         }
@@ -42,6 +43,20 @@ class BackgroundViewController: UIViewController, UITableViewDelegate, UITableVi
         let backButton = UIBarButtonItem(image: #imageLiteral(resourceName: "back").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(popCurrentViewController))
         self.navigationItem.setHidesBackButton(true, animated: true)
         self.navigationItem.leftBarButtonItem = backButton
+        
+        let containerView:UIView = UIView(frame:self.bckTableView.frame)
+        //self.bckTableView = UITableView(frame: containerView.bounds, style: .plain)
+        containerView.backgroundColor = UIColor.clear
+        containerView.layer.shadowColor = UIColor.darkGray.cgColor
+        containerView.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        containerView.layer.shadowOpacity = 1.0
+        containerView.layer.shadowRadius = 2
+        
+        self.bckTableView.layer.cornerRadius = 10
+        self.bckTableView.layer.masksToBounds = true
+        self.view.addSubview(containerView)
+        containerView.addSubview(self.bckTableView)
+        
         bckTableView.delegate = self
         bckTableView.dataSource = self
         DataStore.shared.loadUsers()
