093c47de655e9684432b730d68559d48832586ac
William Lin <wlsaidhi@gmail.com>
2017-11-28 20:08:21 -0600

    added cleanup method to generator

--- Flight Sickness/ObstacleGenerator.swift
+++ Flight Sickness/ObstacleGenerator.swift
@@ -22,13 +22,30 @@ import SpriteKit
  eventaully maybe handle seat generation as well.
  */
 class ObstacleGenerator {
-    // list of possible obstacles to generate
+    // list of possible obstacles types to generate
     let obstaclesTypes: [Obstacle.Type]
+    
+    // list of lanes that obstacles can spawn in
     let lanes: [CGFloat]
+    
+    // keeps track of the # of free instances for each obstacle type
+    var freeCounts = [Metatype<Obstacle>: Int]()
+    
+    // a linked list of free instances for each obstacle type
+    var freeLists = [Metatype<Obstacle>: LinkedList<Obstacle>]()
+    
+    // list of obstacles that are visible (not in a free list)
+    var activeObstacles = LinkedList<Obstacle>()
     
     init(_ obstaclesTypes: [Obstacle.Type], _ lanes: [CGFloat]) {
         self.obstaclesTypes = obstaclesTypes
         self.lanes = lanes
+        
+        // initialize the dictionaries
+        for type in obstaclesTypes {
+            freeCounts[type.metatype] = 0
+            freeLists[type.metatype] = LinkedList<Obstacle>()
+        }
     }
     
     // determines which obstacle should be returned. Does not initialize them.
@@ -44,7 +61,30 @@ class ObstacleGenerator {
             newObstacle.setPosition(x: lanes[lane], y: y + newObstacle.size.height)
         }
         
+        self.activeObstacles.append(newObstacle)
+        
         return newObstacle
+    }
+    
+    /*
+      adds obstacles to the freeLists as they move off of screen.
+      This lets us reuse them by changing their position and adding them back
+      to the scene instead of instantiating new obstacles and letting swift garbage
+      collect.
+     */
+    func collectObstacles(bottomScreen: CGFloat) {
+        var node = self.activeObstacles.first
+        while node != nil {
+            let next = node!.next
+            // if the obstacle is not visible anymore we can collect them
+            if (node!.value.position.y < bottomScreen) {
+                node!.value.removeFromParent()
+                self.activeObstacles.remove(node: node!)
+                self.freeLists[type(of: node!.value).metatype]!.append(node!)
+                self.freeCounts[type(of: node!.value).metatype]! += 1
+            }
+            node = next
+        }
     }
 }
 
