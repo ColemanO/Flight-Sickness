cdcc06e453383d145fc1b66c24ad0375212e48c3
William Lin <wlsaidhi@gmail.com>
2017-12-03 19:43:28 -0600

    added toiletpaper class

---
+++ Flight Sickness/Sprites/ToiletPaper.swift
@@ -0,0 +1,29 @@
+//
+//  ToiletPaper.swift
+//  Flight Sickness
+//
+//  Created by William Lin on 12/3/17.
+//  Copyright © 2017 Coleman. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class ToiletPaper: Obstacle {
+    required init() {
+        let texture = SKTexture(imageNamed: "toiletpaper")
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        // disable bag's collision before it lands
+        self.physicsBody?.categoryBitMask = BitMask.powerUp
+        self.physicsBody?.contactTestBitMask = BitMask.player
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func cleanUp() {
+        // reset the test bit
+        //self.physicsBody?.contactTestBitMask = BitMask.player
+    }
+}
