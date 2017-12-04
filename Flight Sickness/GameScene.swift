b8aeef93c2309e8e028897949e1e768368efd04e
William Lin <wlsaidhi@gmail.com>
2017-12-03 19:02:39 -0600

    added new bitmask bit for power ups

--- Flight Sickness/GameScene.swift
+++ Flight Sickness/GameScene.swift
@@ -14,6 +14,7 @@ import AVFoundation
 struct BitMask {
     static let player: UInt32 = 0x1 << 0
     static let gameStopper: UInt32 = 0x1 << 1
+    static let powerUp: UInt32 = 0x1 << 2
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
